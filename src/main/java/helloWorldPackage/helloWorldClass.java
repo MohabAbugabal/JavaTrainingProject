d092892074c5f73a708e0b2c32096549895e4fbb
mabugabal <you@example.com>
2020-10-15 10:32:18 +0200

    Change in loop numbers

--- src/main/java/helloWorldPackage/helloWorldClass.java
+++ src/main/java/helloWorldPackage/helloWorldClass.java
@@ -5,8 +5,8 @@ public class helloWorldClass {
 
     public static void main(String[] args){
 
-      for(int i = 0 ; i < 4 ; i++) {
-          for (int j = 0; j < 4; j++) {
+      for(int i = 0 ; i < 3 ; i++) {
+          for (int j = 0; j < 3; j++) {
 
               System.out.print("*");
           }
